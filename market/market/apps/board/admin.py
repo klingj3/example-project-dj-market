cf70c483c7fffce6ed86b00dad507a0fd43be43f
John Klingelhofer <jkklingelhofer@gmail.com>
2017-09-25 09:47:15 -0400

    Registered board in settings, added basic post model.

--- market/market/apps/board/admin.py
+++ market/market/apps/board/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from market.apps.board.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    list_display = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
--- market/market/apps/board/models.py
+++ market/market/apps/board/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.CharField(max_length=1023)
+    #price = PriceField('Price', currency='USD', max_digits = 5, decimal_places=2)
+    UNIT_CHOICES = [('Lb', 'LB'),
+                    ('Gallon', 'GALLON'),
+                    ('each', 'EACH'),
+                    ]
+    unit = models.CharField(max_length=7, choices=UNIT_CHOICES, default='each')
+    location = models.TextField(max_length=5)
+    dance=models.CharField(max_length=222)
+    postDate = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True)
