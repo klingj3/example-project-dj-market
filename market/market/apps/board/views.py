52b2a00a5388f183a3ed1564aa62b1a854afe140
John Klingelhofer <jkklingelhofer@gmail.com>
2017-09-25 10:03:14 -0400

    Basic models in place, added url for viewing a model individually if we wanted to.

--- market/market/apps/board/views.py
+++ market/market/apps/board/views.py
@@ -1,6 +1,15 @@
 from django.contrib import messages
+from django.shortcuts import render
 from django.views.generic import TemplateView
+from market.apps.board.models import Post
 
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts,})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_detail.html', {'post':post})
 
 class TestView(TemplateView):
     template_name = 'base.html'
--- market/market/urls.py
+++ market/market/urls.py
@@ -7,9 +7,10 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from market.apps.board.views import TestView
-
+import market.apps.board.views as views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', TestView.as_view()),
+    url(r'^$', TestView.as_view()),
+    url(r'posts/(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
 ]
