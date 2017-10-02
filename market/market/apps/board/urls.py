46b09326b3a978208900ebab8b402f4c87f9411a
Justin Carlson <jrcarlson8@gmail.com>
2017-10-02 11:27:28 -0400

    Post list style and structure improvements, functional pagination

--- market/market/apps/board/urls.py
+++ market/market/apps/board/urls.py
@@ -11,7 +11,7 @@ app_name = 'board'
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='list'),
     url(r'^new/$', PostCreateView.as_view(), name='create'),
-    url(r'^(?P<slug>[-\w]+)/update/', PostUpdateView.as_view(), name='update'),
-    url(r'^(?P<slug>[-\w]+)/delete/', PostDeleteView.as_view(), name='delete'),
-    url(r'^(?P<slug>[-\w]+)/', PostDetailView.as_view(), name='detail'),
+    url(r'^posts/(?P<slug>[-\w]+)/edit/$', PostUpdateView.as_view(), name='update'),
+    url(r'^posts/(?P<slug>[-\w]+)/delete/$', PostDeleteView.as_view(), name='delete'),
+    url(r'^posts/(?P<slug>[-\w]+)/$', PostDetailView.as_view(), name='detail'),
 ]
