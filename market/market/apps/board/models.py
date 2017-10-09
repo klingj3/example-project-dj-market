d8a6ccb283b1b5d05b2d3c6e919b84011a15bd91
Justin Carlson <jrcarlson8@gmail.com>
2017-10-09 17:25:07 -0400

    Initial model and form for post images, other revisions

--- market/market/apps/board/models.py
+++ market/market/apps/board/models.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 from django_extensions.db.models import (ActivatorModel,
                                          TimeStampedModel)
-from django_prices.models import PriceField
 from geoposition.fields import GeopositionField
 from tagulous.models import TagField
 
@@ -17,7 +17,7 @@ class Post(RandomSlugModel, ActivatorModel, TimeStampedModel):
         ('each', 'EACH'),
     )
 
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # todo: published field
     # todo: Remove activatormodel?
@@ -26,10 +26,18 @@ class Post(RandomSlugModel, ActivatorModel, TimeStampedModel):
     body = models.TextField(max_length=5000)
 
     # TODO: Use autocomplete_initial=True and specify preset tags
-    tags = TagField(max_count=10, force_lowercase=True, space_delimiter=False)
+    tags = TagField(max_count=10, force_lowercase=True, space_delimiter=False, blank=True)
 
-    price = PriceField(currency='USD', max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
     unit = models.CharField(max_length=80, choices=UNIT_CHOICES, default='each')
 
     # location = models.CharField(max_length=5)
     location = GeopositionField()
+
+    def get_absolute_url(self):
+        return reverse('board:detail', kwargs={'slug': self.slug})
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(blank=True)
--- market/market/apps/board/views.py
+++ market/market/apps/board/views.py
@@ -8,20 +8,41 @@ from django.views.generic import (CreateView,
                                   TemplateView,
                                   UpdateView)
 
+from extra_views import (CreateWithInlinesView,
+                         InlineFormSet)
+
 from market.apps.board.forms import PostForm
-from market.apps.board.models import Post
+from market.apps.board.models import (Post,
+                                      PostImage)
 from market.apps.core.mixins import (CreateWithOwnerMixin,
                                      OwnerRequiredMixin)
 
 
-class PostCreateView(CreateWithOwnerMixin, CreateView):
+# class PostCreateView(CreateWithOwnerMixin, CreateView):
+#     model = Post
+#     form_class = PostForm
+#     template_name = 'board/post_form.html'
+#
+#     def get_success_url(self):
+#         messages.success(self.request, 'Posting successfully created!', extra_tags='fa fa-check')
+#         return reverse('board:list')
+
+
+class ImagesInline(InlineFormSet):
+    model = PostImage
+    fields = ['image']
+    # extra = 5
+    max_num = 2
+
+
+class PostCreateView(CreateWithOwnerMixin, CreateWithInlinesView):
     model = Post
     form_class = PostForm
     template_name = 'board/post_form.html'
+    inlines = [ImagesInline]
 
     def get_success_url(self):
-        messages.success(self.request, 'Posting successfully created!', extra_tags='fa fa-check')
-        return reverse('board:list')
+        return self.object.get_absolute_url()
 
 
 class PostDeleteView(OwnerRequiredMixin, DeleteView):
