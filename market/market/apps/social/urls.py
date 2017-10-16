6bee2a403439b21336d87898c818a424763498f4
John Klingelhofer <jkklingelhofer@gmail.com>
2017-10-16 13:37:45 -0400

    Basic profile page with button to edit.

--- market/market/apps/social/urls.py
+++ market/market/apps/social/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^$', UserProfileListView.as_view(), name='list'),
     url(r'^new/$', UserProfileCreateView.as_view(), name='create'),
     url(r'^me/', SelfRedirectView.as_view(), name='me'),
-    url(r'^(?P<slug>[-\w]+)/edit/$', UserProfileUpdateView.as_view(), name='update'),
+    url(r'^(?P<slug>\w+)/edit/$', UserProfileUpdateView.as_view(), name='update'),
     url(r'^(?P<slug>\w+)/$', UserProfileDetailView.as_view(), name='detail'),
 ]
