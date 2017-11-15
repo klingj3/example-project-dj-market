137ee9a0622587aae83c8ad0cc04668170db708a
Justin Carlson <jrcarlson8@gmail.com>
2017-11-14 22:59:59 -0500

    Implement image upload and display

--- market/market/urls.py
+++ market/market/urls.py
@@ -3,7 +3,9 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -25,3 +27,6 @@ urlpatterns = [
     url(r'^', include('market.apps.board.urls')),
     
 ]
+
+# URL for media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
